bdd03cfe5b4b92fd217166e0b72f5870d83e22c0
Maximus Wetterholm-Österman <maximus@oesterman.com>
2025-02-17 16:04:50 +0100

    started single_element_list test

--- lab2/list_test.cc
+++ lab2/list_test.cc
@@ -6,14 +6,16 @@ TEST_CASE ("Constructors") {
 
     SECTION("DEFAULT") {
         List empty_list;
-        List single_elemnt_list{1};
+        List single_element_list{1};
         List initialized_list{2,5,7};
         std::ostringstream outputStream;
 
         empty_list.insert(1);
         outputStream << empty_list;  // Capture stream output
-
         CHECK(outputStream.str() == "{}");  // Expected sorted output
+
+        outputStream << single_element_list;  // Capture stream output
+        CHECK(outputStream.str() == "{1}");  // Expected sorted output
 
     }
 
